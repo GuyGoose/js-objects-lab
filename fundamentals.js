e9d41a040990ee159d56b548f08d61a51b518983
Anthony O Keeffe <antokeeffe@outlook.ie>
2022-09-15 12:36:06 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,10 @@
 const aCar = {
     owner : "Joe Bloggs",
+    address : '3 Walkers Lane',
+    previous_owners: [
+        { name : 'Pat Smith', address : '1 Main Street'},
+        { name : 'Sheila Dwyer', address : '2 High Street'}
+      ],
     type : { 
         make: 'Toyota',
         model: 'Corolla',
@@ -20,8 +25,11 @@ const aCar = {
         shade : 'cream'
     }
   }
+
+
   
   console.log(aCar.owner);
   console.log(aCar.owner + ' drives a ' + aCar.type.make)
   console.log('Reg. = ' + aCar.registration['year'] + '-' + aCar.registration['code'] + '-' + aCar.registration['number']);
   console.log('It is a ' + aCar.colour['exterior'] + ' car, ' + aCar.mileage + ' mileage, with a ' + aCar.colour.interior['texture'] + ' interior.')
+  console.log('First owner : ' + aCar.previous_owners[0].name )

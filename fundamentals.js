1b31e6979e85eec8d9b6ff0f8d66634dadb1499f
Anthony O Keeffe <antokeeffe@outlook.ie>
2022-09-15 12:24:48 +0100

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -11,7 +11,17 @@ const aCar = {
         number: '1058',
     }
   };
+
+  aCar.mileage = 10000 ;
+  aCar.colour = {
+    exterior : 'Red',
+    interior : {
+        texture : 'leather',
+        shade : 'cream'
+    }
+  }
   
   console.log(aCar.owner);
   console.log(aCar.owner + ' drives a ' + aCar.type.make)
   console.log('Reg. = ' + aCar.registration['year'] + '-' + aCar.registration['code'] + '-' + aCar.registration['number']);
+  console.log('It is a ' + aCar.colour['exterior'] + ' car, ' + aCar.mileage + ' mileage, with a ' + aCar.colour.interior['texture'] + ' interior.')
